3f707c2dc0240e8b932a2b9e6423e13f48b5de2f
tejaswi <tejaswivj@gmail.com>
2016-01-26 18:31:08 -0600

    test1

--- String/src/com/string/app/StringCompareEmp.java
+++ String/src/com/string/app/StringCompareEmp.java
@@ -3,7 +3,7 @@ package com.string.app;
 public class StringCompareEmp {
 public static void main (String args[]){
 	String str = "Hello World";
-    String anotherString = "hello world";
+    String anotherString = "hello world  ";
     Object objStr = str;
 
     System.out.println( str.compareToIgnoreCase(anotherString) );
